a01c97549df740c54cdc3de6a4203e3194f9d4b2
heliu-River <>
2017-10-26 08:26:36 +0800

    view

--- easyPHP/system/ezRoute.php
+++ easyPHP/system/ezRoute.php
@@ -13,58 +13,29 @@ class ezRoute
 	static private $entryFileName		= 'index.php/';
 	static private $controllerPath		=  '/controller/';
 	static private $suffix				= '.php';
-	public function analyseRoute($route = null)
-	{
-		if ($route == null) {
-			$url = explode(self::$entryFileName, $_SERVER['REQUEST_URI'])[1];
-		} else
-			$url = $route;
+	public function analyseRoute($route = null){
+		if ($route == null) $url = explode(self::$entryFileName, $_SERVER['REQUEST_URI'])[1];
+        else $url = $route;
 		// 解析url
 		$urlArray	= explode('/', $url);
 		$controlName = $urlArray[0];
 		$methonName	= $urlArray[1];
 		$param		 = array();
-		for ($i = 2; $i < count($urlArray); $i++) {
-			$param[] = $urlArray[$i];
-		}
+		for ($i = 2; $i < count($urlArray); $i++) $param[] = $urlArray[$i];
 		$file =ezAPPPATH . self::$controllerPath . $controlName . self::$suffix;
-		if (!file_exists($file))
-			throw new Exception("没有这个控制器");
-
+		if (!file_exists($file)) throw new Exception("没有这个控制器");
 		require_once $file;
-		if (!class_exists($controlName))
-			throw new Exception("没有这个类");
-
-		if (get_parent_class($controlName) != 'ezControl')
-			throw new Exception("类没有继承ezControl");
-
-
+		if (!class_exists($controlName)) throw new Exception("没有这个类");
+		if (get_parent_class($controlName) != 'ezControl') throw new Exception("类没有继承ezControl");
 		$method = new ReflectionMethod($controlName, $methonName);
-
-		if (!$method->isPublic() || $method->isStatic())
-			throw new Exception("函数不是public或是静态");
-
-		return array(
-			'control' => $controlName,
-			'methon' => $methonName,
-			'param' => $param
-		);
+		if (!$method->isPublic() || $method->isStatic()) throw new Exception("函数不是public或是静态");
+		return array('control' => $controlName, 'methon' => $methonName, 'param' => $param);
 	}
 
-	public function executeRoute($dispatch)
-	{
-		if (ezFilter($dispatch, array(
-			'control',
-			'methon',
-			'param'
-		)) != true)
-			throw new Exception("配置文件错误");
-
-		$method = new ReflectionMethod($dispatch['control'], $dispatch['methon']);
-
-		// 获取这个函数参数类型定义
-		$instance = new $dispatch['control']();
-		$instance->setDispatch($dispatch);
-		return $method->invokeArgs($instance, $dispatch['param']);
+	public function executeRoute($dispatch){
+		if (ezFilter($dispatch, array('control', 'methon', 'param')) != true) throw new Exception("配置文件错误");
+        $control = new $dispatch['control']();
+        $control->setDispatch($dispatch);
+       return runG(array($control,$dispatch['methon']),$dispatch['param']);
 	}
 }
